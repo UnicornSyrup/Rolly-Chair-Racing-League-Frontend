59c7e20bfc3af39b520e155f6af0057f224df271
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2023-11-05 09:54:53 -0600

    logo

--- Rolly-Chair-Racing-League/src/components/Header.vue
+++ Rolly-Chair-Racing-League/src/components/Header.vue
@@ -3,17 +3,36 @@
 
 <template>
     <div id="header-container">
-        <h1><b>Roll With Us</b></h1>
+        <img src="../assets/logo.png" alt=""><h1><b>Roll With Us</b></h1>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 #header-container{
     height: 15vh;
     width: 100vw;
     border-color: white;
     background: darkslateblue;
+
+    img{
+        height: 90%;
+    }
+
+    h1{
+
+        display: inline;
+        margin-left: 30vw;
+    }
+    
 }
+
+// #header-container {
+//     height: 10vh;
+//     /* position: relative; */
+//     img{
+//         h
+//     }
+// }
 
 h1 {
     color: white;
