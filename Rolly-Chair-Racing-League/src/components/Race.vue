4af0ca1382d6802b774737f74d2cc966c849088b
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2023-11-04 20:00:45 -0500

    initial components

---
+++ Rolly-Chair-Racing-League/src/components/Race.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Track from './Track.vue';
+</script>
+
+<template>
+    <div id="race-container">
+        <Track></Track>
+    </div>
+</template>
+
+<style scoped lang="scss">
+ #race-container{
+    width:300px;
+    height: 400px;
+    // background: #0088ee;
+} 
+</style>
---
+++ Rolly-Chair-Racing-League/src/components/Track.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div id="track-container">
+
+    </div>
+</template>
+
+<style scoped>
+#track-container{
+    height: 80vh;
+    width: 50vw;
+    border-color: white;
+    background: dodgerblue;
+}
+</style>
